927b43aca3eff29e0abfb6ddda35957076489649
Louis Pontoise <louis.pontoise@amadeus.com>
2017-08-27 23:52:50 +0200

    Added Travis build to do rustfmt, build, and test

--- src/Config.rs
+++ src/Config.rs
@@ -2,14 +2,14 @@
 pub struct Config {
     /// List of absolute paths of the files/folders to watch (recursively)
     /// e.g.  ["/path/config.conf", "/path/config-directory/"]
-    files_to_watch: Vec<String>,
+    pub files_to_watch: Vec<String>,
     /// Absolute path to the Git repository where the files are copied and committed
     /// e.g. /path/git_clones/repo
-    destination_repo: String,
+    pub destination_repo: String,
     /// Name of the remote on the Git repository where the files are copied and committed
     /// e.g. "origin"
-    remote: String,
+    pub remote: String,
     /// Git references
     /// e.g. "refs/heads/master:refs/heads/master"
-    refs: String,
+    pub refs: String,
 }
--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,28 @@ extern crate log;
 extern crate env_logger;
 extern crate serde;
 extern crate serde_json;
+extern crate git2;
+extern crate time;
+extern crate notify;
 
 mod config;
 
 use std::fs::File;
 use std::path::Path;
 use std::error::Error;
+use git2::{Repository, Direction};
 use config::Config;
 
-fn read_config_from_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<Error>> {
-    let file = File::open(path)?;
-    let config = serde_json::from_reader(file)?;
-    Ok(config)
+fn config_from_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<Error>> {
+    Ok(serde_json::from_reader(File::open(path)?)?)
 }
 
 fn do_stuff() -> Result<(), Box<Error>> {
-    let config = read_config_from_file("resources/config.json")?;
-    println!("{:#?}", config);
+    let config = config_from_file("resources/config.json")?;
+    info!("{:#?}", config);
+    let repo = Repository::open(&config.destination_repo)?;
+    add_all_and_commit_and_push(repo, config)?;
     Ok(())
-
     //    AutoCommit.REPO = await nodegit.Repository.open(AutoCommit.CONFIG.destinationRepo)
     //    chokidar.watch(AutoCommit.CONFIG.filesToWatch)
     //        .on('add', path => AutoCommit.add(path))
@@ -32,6 +35,31 @@ fn do_stuff() -> Result<(), Box<Error>> {
     //    .on('unlink', path => AutoCommit.remove(path))
     //    .on('unlinkDir', path => AutoCommit.remove(path))
     //    .on('error', error => { throw error })
+}
+
+fn add_all_and_commit_and_push(repo: Repository, config: Config) -> Result<(), Box<Error>> {
+    // commit
+    let mut index = repo.index()?;
+    index.add_path(Path::new("."))?;
+    let oid = index.write_tree()?;
+    let tree = repo.find_tree(oid)?;
+    let message = format!("Autocommit {}", time::now().rfc822());
+    let head = repo.find_commit(repo.head().unwrap().target().unwrap())
+        .unwrap();
+    let signature = repo.signature()?;
+    repo.commit(
+        Some("HEAD"),
+        &signature,
+        &signature,
+        &message,
+        &tree,
+        &[&head],
+    )?;
+    // push
+    let mut remote = repo.find_remote(&config.remote)?;
+    remote.connect(Direction::Push)?;
+    remote.push(&[&config.refs], None)?;
+    Ok(())
 }
 
 fn main() {
